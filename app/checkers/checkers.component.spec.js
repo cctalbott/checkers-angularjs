22f36d916563a74e47931ed8a034cd12d62740b0
Clint Talbott <cctalbott@gmail.com>
2016-11-10 12:29:59 -0600

    Added base test for factories

--- app/checkers/checkers.component.spec.js
+++ app/checkers/checkers.component.spec.js
@@ -5,6 +5,42 @@ describe('checkers', function() {
   // Load the module that contains the `checkers` component before each test
   beforeEach(module('checkers'));
   
+  // Test factories
+  describe('Checker', function() {
+    it('can get an instance of `Checker` factory', inject(function(Checker) {
+      expect(Checker).toBeDefined();
+      expect(Checker.colors).not.toBe(null);
+      expect(Checker.colors.length).toBe(3);
+      expect(Checker.colors).toContain("red");
+      expect(Checker.colors).toContain("black");
+      expect(Checker.colors).toContain("empty");
+    }));
+  });
+  
+  describe('Checkers', function() {
+    it('can get an instance of `Checkers` factory', inject(function(Checkers) {
+      expect(Checkers).toBeDefined();
+    }));
+  });
+  
+  describe('Board', function() {
+    it('can get an instance of `Board` factory', inject(function(Board) {
+      expect(Board).toBeDefined();
+      expect(Board.rows).not.toBe(null);
+      expect(Board.cols).not.toBe(null);
+      expect(Board.checkers).not.toBe(null);
+      expect(Board.rows.length).toBe(8);
+      expect(Board.cols.length).toBe(8);
+    }));
+  });
+  
+  describe('CheckWins', function() {
+    it('can get an instance of `CheckWins` factory', inject(function(CheckWins) {
+      expect(CheckWins).toBeDefined();
+      expect(CheckWins.wins).toBeDefined();
+    }));
+  });
+  
   // Test the controller 
   describe('CheckersController', function(){
     var ctrl;
@@ -30,16 +66,5 @@ describe('checkers', function() {
     it('should create a `wins` object', function() {
       expect(ctrl.board.wins).not.toBe(null);
     });
-  });
-  
-  describe('Checker', function() {
-    it('can get an instance of `Checker` factory', inject(function(Checker) {
-      expect(Checker).toBeDefined();
-      expect(Checker.colors).not.toBe(null);
-      expect(Checker.colors.length).toBe(3);
-      expect(Checker.colors).toContain("red");
-      expect(Checker.colors).toContain("black");
-      expect(Checker.colors).toContain("empty");
-    }));
   });
 });
